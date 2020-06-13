446f922c3af4039efe2c16bb37d3fd1eed54a062
Vezzalinistefano <vezzalini.stefano@yahoo.it>
2020-06-13 10:50:55 +0200

    13-06-2020

--- esercizi_nuovo_pdf/es8.m
+++ esercizi_nuovo_pdf/es8.m
@@ -1,8 +1,14 @@
-clear all
-close all
+clear all 
+close all 
 
-d = .003;
-x = (1-d:0.0001:1+d);
-p = x.^6 - 6.*x.^5 +15.*x.^4 -20.*x.^3 +15.*x.^2 -6.*x +1;
 
-plot(p, '-b', 'LineWidth', 1.5);
+d = [0.1 0.01 0.008 0.007 0.005 0.003] ; 
+
+
+
+for i = 1 : 6 
+    x=[1-d(i):0.0001:1+d(i)]; 
+    p = x.^6 - 6*x.^5 + 15*x.^4 - 20*x.^3 + 15*x.^2 - 6*x + 1 ; 
+    subplot(2,3,i) ; plot(p, '-b', 'LineWidth', 1.5); 
+    title(i)
+end
